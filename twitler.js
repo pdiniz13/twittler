9e0b33aaab59f78db6a34a48b9e2d879095ef796
Paulo Diniz <pdiniz13@gmail.com>
2015-01-08 10:00:59 -0800

    completed first basic requirement

--- twitler.js
+++ twitler.js
@@ -5,15 +5,21 @@ $(document).ready(function(){
     var $body = $('body');
     var $users = [];
     var $streams = {};
-    $body.html('');
-
-    var index = streams.home.length - 1;
-    while(index >= 0){
-        var tweet = streams.home[index];
-        var $tweet = $('<div></div>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message);
-        $tweet.appendTo($body);
-        index -= 1;
+    //$body.html('');
+    function displayTweets(){
+        $('.tweets').html('');
+        var index = streams.home.length - 1;
+        while(index >= 0){
+            var tweet = streams.home[index];
+            var $tweet = $('<div></div>');
+            $tweet.text('@' + tweet.user + ': ' + tweet.message);
+            $tweet.appendTo('.tweets');
+            index -= 1;
+        }
     }
-
+    displayTweets();
+    $('button').on('click', function(e){
+        e.preventDefault();
+        displayTweets();
+    });
 });
